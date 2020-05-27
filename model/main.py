46053baf3f1edd5a2d44a4b403194c3f32fdede3
Nicolai Garcia <nicogarcia@Nicolais-MBP.attlocal.net>
2020-05-27 15:17:50 -0700

    Trained on Landscape paintings, then abstract paintings. Results are better.

--- model/main.py
+++ model/main.py
@@ -28,14 +28,15 @@ output_image_size = 64 # size of output images
 lr = 2e-4
 beta_1 = 0.5
 num_samples = 64 # number of samples to generate at each epoch
-epochs = 25
+epochs = 100
 # logistics
-save_every = 1
+restore_from_checkpoint=True
+save_every = 25
 # Directories
 data_path = "../"
-dataset_name = "abstract-dataset-small"
+dataset_name = "Abstract"
 checkpoint_dir = "./training_checkpoints"
-trial_nr = 11 # which training trial this is
+trial_nr = 13 # which training trial this is
 output_dir = "../images/trial_" + str(trial_nr)
 
 
@@ -43,7 +44,7 @@ output_dir = "../images/trial_" + str(trial_nr)
 
 print("loading data...")
 train_images = util.load_data(data_path, dataset_name)
-print(len(train_images))
+print(len(train_images), "images loaded.")
 
 # In[4]:
 
@@ -52,32 +53,32 @@ print(len(train_images))
 
 # Generator
 def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(ngf*8*4*4, use_bias=False, input_shape=(nz,)))
-    model.add(layers.Reshape((4, 4, ngf*8)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
-    assert model.output_shape == (None, 4, 4, ngf*8) # Note: None is the batch size
+	model = tf.keras.Sequential()
+	model.add(layers.Dense(ngf*8*4*4, use_bias=False, input_shape=(nz,)))
+	model.add(layers.Reshape((4, 4, ngf*8)))
+	model.add(layers.BatchNormalization())
+	model.add(layers.ReLU())
+	assert model.output_shape == (None, 4, 4, ngf*8) # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(ngf*4, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 8, 8, ngf*4)
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
+	model.add(layers.Conv2DTranspose(ngf*4, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 8, 8, ngf*4)
+	model.add(layers.BatchNormalization())
+	model.add(layers.ReLU())
 
-    model.add(layers.Conv2DTranspose(ngf*2, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 16, 16, ngf*2)
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
-    
-    model.add(layers.Conv2DTranspose(ngf*1, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 32, 32, ngf)
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
+	model.add(layers.Conv2DTranspose(ngf*2, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 16, 16, ngf*2)
+	model.add(layers.BatchNormalization())
+	model.add(layers.ReLU())
+	
+	model.add(layers.Conv2DTranspose(ngf*1, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 32, 32, ngf)
+	model.add(layers.BatchNormalization())
+	model.add(layers.ReLU())
 
-    model.add(layers.Conv2DTranspose(nc, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 64, 64, nc)
+	model.add(layers.Conv2DTranspose(nc, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+	assert model.output_shape == (None, 64, 64, nc)
 
-    return model
+	return model
 
 
 # In[5]:
@@ -97,31 +98,31 @@ print(generated_image.shape)
 
 # Discriminator
 def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(ndf, (5, 5), strides=(2, 2), padding='same', input_shape=[64, 64, nc]))
-    model.add(layers.LeakyReLU())
-    assert model.output_shape == (None, 32, 32, ndf)
+	model = tf.keras.Sequential()
+	model.add(layers.Conv2D(ndf, (5, 5), strides=(2, 2), padding='same', input_shape=[64, 64, nc]))
+	model.add(layers.LeakyReLU())
+	assert model.output_shape == (None, 32, 32, ndf)
 
-    model.add(layers.Conv2D(ndf*2, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    assert model.output_shape == (None, 16, 16, ndf*2)
+	model.add(layers.Conv2D(ndf*2, (5, 5), strides=(2, 2), padding='same'))
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU())
+	assert model.output_shape == (None, 16, 16, ndf*2)
 
-    model.add(layers.Conv2D(ndf*4, (5, 5), strides=(2, 2), padding='same'))
-    print(model.output_shape)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    assert model.output_shape == (None, 8, 8, ndf*4)
+	model.add(layers.Conv2D(ndf*4, (5, 5), strides=(2, 2), padding='same'))
+	print(model.output_shape)
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU())
+	assert model.output_shape == (None, 8, 8, ndf*4)
 
-    model.add(layers.Conv2D(ndf*8, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    assert model.output_shape == (None, 4, 4, ndf*8)
-    
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
+	model.add(layers.Conv2D(ndf*8, (5, 5), strides=(2, 2), padding='same'))
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU())
+	assert model.output_shape == (None, 4, 4, ndf*8)
+	
+	model.add(layers.Flatten())
+	model.add(layers.Dense(1))
 
-    return model
+	return model
 
 
 # In[7]:
@@ -143,13 +144,13 @@ cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 # Define losses
 def discriminator_loss(real_output, fake_output):
-    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
-    total_loss = real_loss + fake_loss
-    return total_loss
+	real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+	fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+	total_loss = real_loss + fake_loss
+	return total_loss
 
 def generator_loss(fake_output):
-    return cross_entropy(tf.ones_like(fake_output), fake_output)
+	return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 # Optimizers
 generator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1)
@@ -162,9 +163,9 @@ discriminator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1)
 # Save checkpoints
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
+								 discriminator_optimizer=discriminator_optimizer,
+								 generator=generator,
+								 discriminator=discriminator)
 
 
 # In[11]:
@@ -182,51 +183,52 @@ seed = tf.random.normal([num_samples, nz])
 # This annotation causes the function to be "compiled".
 @tf.function
 def train_step(images):
-    noise = tf.random.normal([batch_size, nz])
+	noise = tf.random.normal([batch_size, nz])
 
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        generated_images = generator(noise, training=True)
+	with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+		generated_images = generator(noise, training=True)
 
-        real_output = discriminator(images, training=True)
-        fake_output = discriminator(generated_images, training=True)
+		real_output = discriminator(images, training=True)
+		fake_output = discriminator(generated_images, training=True)
 
-        gen_loss = generator_loss(fake_output)
-        disc_loss = discriminator_loss(real_output, fake_output)
+		gen_loss = generator_loss(fake_output)
+		disc_loss = discriminator_loss(real_output, fake_output)
 
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+	gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+	gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-    
-    return gen_loss, disc_loss
+	generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+	discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+	
+	return gen_loss, disc_loss
 
 def train(dataset, epochs):
-    for epoch in range(epochs):
-        start = time.time()
-        batch_idxs = len(dataset) // batch_size # number of batches in train dataset
-        for idx in range(batch_idxs):
-            step_time = time.time()
-            image_batch = util.get_images(dataset[idx*batch_size:(idx+1)*batch_size])
-            gen_loss, disc_loss = train_step(image_batch)
-            print("Epoch: [{}/{}] [{}/{}] took: {:.3f}, d_loss: {:.5f}, g_loss: {:.5f}".format(
-                epoch+1, epochs, idx, batch_idxs, time.time()-step_time, disc_loss, gen_loss))
+	for epoch in range(75, epochs):
+		start = time.time()
+		batch_idxs = len(dataset) // batch_size # number of batches in train dataset
+		for idx in range(batch_idxs):
+			step_time = time.time()
+			image_batch = util.get_images(dataset[idx*batch_size:(idx+1)*batch_size])
+			gen_loss, disc_loss = train_step(image_batch)
+			print("Epoch: [{}/{}] [{}/{}] took: {:.3f}, d_loss: {:.5f}, g_loss: {:.5f}".format(
+				epoch+1, epochs, idx+1, batch_idxs, time.time()-step_time, disc_loss, gen_loss))
 
-        # Produce images for the GIF as we go
-        generate_and_save_images(generator,
-                                 epoch + 1,
-                                 seed)
+		# Produce images for the GIF as we go
+		generate_and_save_images(generator,
+								 epoch + 1,
+								 seed)
 
-        # Save the model every 15 epochs
-        if (epoch + 1) % save_every == 0:
-            checkpoint.save(file_prefix = checkpoint_prefix)
+		# Save the model after every number of epochs
+		if (epoch+1) % save_every == 0:
+			print("saving checkpoint at", epoch+1)
+			checkpoint.save(file_prefix = checkpoint_prefix)
 
-        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+		print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
-    # Generate after the final epoch
-    generate_and_save_images(generator,
-                           epochs,
-                           seed)
+	# Generate after the final epoch
+	generate_and_save_images(generator,
+						   epochs,
+						   seed)
 
 
 # In[13]:
@@ -234,31 +236,27 @@ def train(dataset, epochs):
 
 # Generate and save images
 def generate_and_save_images(model, epoch, test_input):
-    # Notice `training` is set to False.
-    # This is so all layers run in inference mode (batchnorm).
-    predictions = model(test_input, training=False)
+	# Notice `training` is set to False.
+	# This is so all layers run in inference mode (batchnorm).
+	predictions = model(test_input, training=False)
 
-    fig = plt.figure(figsize=(8,8))
+	fig = plt.figure(figsize=(8,8))
 
-    for i in range(predictions.shape[0]):
-        plt.subplot(8, 8, i+1)
-        plt.imshow((predictions.numpy()[i, :, :, :] * 127.5 + 127.5).astype(int)) # put image on [0,255] range
-        plt.axis('off')
+	for i in range(predictions.shape[0]):
+		plt.subplot(8, 8, i+1)
+		plt.imshow((predictions.numpy()[i, :, :, :] * 127.5 + 127.5).astype(int)) # put image on [0,255] range
+		plt.axis('off')
 
-    plt.savefig(output_dir + '/' + 'image_at_epoch_{:04d}.png'.format(epoch))
-    plt.clf()
-
-
-# In[14]:
-
-
-# checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir)).assert_consumed()
-
+	plt.savefig(output_dir + '/' + 'image_at_epoch_{:04d}.png'.format(epoch))
+	plt.clf()
 
 # In[ ]:
 
 
 # Train the model
+if restore_from_checkpoint:
+	checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
 train(train_images, epochs)
 
 
--- model/util.py
+++ model/util.py
@@ -3,16 +3,37 @@ from PIL import Image
 import numpy as np
 import os
 from glob import glob
+import csv
 
 # get all image files in directory specified by from_path and store resized images into to_path
-def resize_and_save_images(from_path, to_path):
-	for filename in listdir(from_path):
-		if filename.endswith(".jpg"):
-			# resize image
-			image = Image.open(from_path + '/' + filename)
-			image_resized = image.resize((64, 64))
-			# save in new directory
-			image_resized.save(to_path + '/' + filename)
+def resize_and_save_images(from_path, save_path, fname_pattern="*.jpg", output_size=64):
+	img_files = glob(os.join(from_path, fname_pattern))
+	for filename in img_files:
+		# resize image
+		image = Image.open(filename)
+		image = image.resize((output_size, output_size))
+		# save in new directory
+		image.save(os.join(save_path, os.path.basename(filename)))
+
+# resize and save a singular image at file_path to save_path directory
+def resize_and_save_image(file_path, save_path, output_size=64):
+	filename = os.path.basename(file_path)
+	print('processing', filename)
+	image = Image.open(file_path)
+	image = image.resize((output_size, output_size))
+	image.save(os.path.join(save_path, filename))
+
+def get_artworks_by_genre(genre, database_path, csv_path, save_path, output_size=64):
+	print('Database', database_path)
+	print('CSV', csv_path)
+	print('Save', save_path)
+	# open csv file
+	with open(csv_path) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		for row in csv_reader:
+			# if this painting is part of the specified genre, resize and save in target directory
+			if int(row[1]) == genre:
+				resize_and_save_image(os.path.join(database_path, row[0]), save_path)
 
 def load_data(data_path, dataset_name, fname_pattern='*.jpg'):
 	path_name = os.path.join(data_path, dataset_name, fname_pattern)
@@ -25,7 +46,11 @@ def get_images(image_files):
 		images.append((img_data - 127.5)/127.5)
 	return np.array(images, dtype='float32')
 
-filenames = load_data('../', 'test-dataset')
-images = get_images(filenames)
-print(images.shape)
-print(images[0])
+
+# CODE FOR CREATING A FOLDER OF ALL PAINTINGS BELONGING TO A GENRE
+# wikiart_data_path = '../../wikiart-data'
+
+# get_artworks_by_genre(genre=4, 
+# 	database_path=os.path.join(wikiart_data_path,'wikiart'), 
+# 	csv_path=os.path.join(wikiart_data_path,'wikiart_csv/genre_train.csv'), 
+# 	save_path=os.path.join(wikiart_data_path,'Landscapes'))
