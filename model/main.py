bbf4194bd69a2e9df37cbd7fae5ea44f74bddf33
Nicolai Garcia <nicogarcia@Nicolais-MBP.attlocal.net>
2020-06-07 09:34:51 -0700

    Code for declaring flags. Trained on augmented abstract dataset.

--- model/main.py
+++ model/main.py
@@ -1,146 +1,114 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import tensorflow as tf
 from tensorflow.keras import layers, activations
 import numpy as np
 import matplotlib.pyplot as plt
 import PIL
 import os
-import time
+from time import time, ctime
 import util
 
-
-# In[2]:
-
-
+flags = tf.compat.v1.flags
+FLAGS = flags.FLAGS
 # Define layer dimensions for model
-ngf = 64 # number of generative filters in first convolutional layer
-ndf = 64 # number of discriminative filters in first convolutional layer
-nc = 3 # number of channels in output (3 for RGB)
-nz = 100 # number of dimensions for noise input to generator
+flags.DEFINE_integer("ngf", 150, "number of generative filters in first convolutional layer")
+flags.DEFINE_integer("ndf", 50, "number of discriminative filters in first convolutional layer")
+flags.DEFINE_integer("nc", 3, "number of channels in output (3 for RGB image)")
+flags.DEFINE_integer("nz", 100, "number of dimensions for noise input to generator")
 # Hyperparameters
-batch_size = 64
-output_image_size = 64 # size of output images
-lr = 2e-4
-beta_1 = 0.5
-num_samples = 64 # number of samples to generate at each epoch
-epochs = 100
-# logistics
-restore_from_checkpoint=True
-save_every = 25
+flags.DEFINE_integer("batch_size", 64, "number of images in each training batch")
+flags.DEFINE_integer("output_image_size", 128, "size of output images")
+flags.DEFINE_float("lr", 2e-4, "learning rate")
+flags.DEFINE_float("beta_1", 0.5, "beta 1 for Adam optimizer")
+flags.DEFINE_float("alpha", 0.2, "alpha for LeakyReLU in Discriminator")
+flags.DEFINE_integer("num_samples", 64, "number of samples to generate after each epoch")
+flags.DEFINE_integer("start_epoch", 0, "Epoch to start from (use if restoring from checkpoint to continue training.")
+flags.DEFINE_integer("epochs", 100, "number of epochs to train for")
+# logistics for training
+flags.DEFINE_bool("train", False, "Whether or not to train the model")
+flags.DEFINE_bool("generate", False, "If true, use the model to generate num_samples images")
+flags.DEFINE_bool("restore_from_checkpoint", False, "determine whether or not to restore from the most recent checkpoint")
+flags.DEFINE_integer("save_every", 25, "save training checkpoints every number of epochs")
 # Directories
-data_path = "../"
-dataset_name = "Abstract"
-checkpoint_dir = "./training_checkpoints"
-trial_nr = 13 # which training trial this is
-output_dir = "../images/trial_" + str(trial_nr)
-
-
-# In[3]:
-
-print("loading data...")
-train_images = util.load_data(data_path, dataset_name)
-print(len(train_images), "images loaded.")
-
-# In[4]:
-
-
-# Create the models
+flags.DEFINE_string("data_path", "../", "Path to the directory where the dataset is stored")
+flags.DEFINE_string("dataset", "Abstract", "Name of the dataset to be used")
+flags.DEFINE_string("checkpoint_dir", "./training_checkpoints", "Path to directory to store training checkpoints")
+flags.DEFINE_string("output_dir", "../images/", "Path to directory to store sample outputs after every epoch")
+flags.DEFINE_string("generated_dir", None, "Specify the directory for images generated from model.")
 
 # Generator
 def make_generator_model():
+	# Number of filters, input and output dimensions
+	ngf = FLAGS.ngf
+	nz = FLAGS.nz
+	nc = FLAGS.nc
+	# Reshape layer (nz,) -> (4,4,ngf*16)
 	model = tf.keras.Sequential()
-	model.add(layers.Dense(ngf*8*4*4, use_bias=False, input_shape=(nz,)))
-	model.add(layers.Reshape((4, 4, ngf*8)))
+	model.add(layers.Dense(ngf*16*4*4, use_bias=False, input_shape=(nz,)))
+	model.add(layers.Reshape((4, 4, ngf*16)))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	assert model.output_shape == (None, 4, 4, ngf*8) # Note: None is the batch size
-
-	model.add(layers.Conv2DTranspose(ngf*4, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-	assert model.output_shape == (None, 8, 8, ngf*4)
+	assert model.output_shape == (None, 4, 4, ngf*16) # Note: None is the batch size
+	# First convolutional layer: (4,4,ngf*16) -> (8,8, ngf*8)
+	model.add(layers.Conv2DTranspose(ngf*8, (4, 4), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 8, 8, ngf*8)
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-
-	model.add(layers.Conv2DTranspose(ngf*2, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-	assert model.output_shape == (None, 16, 16, ngf*2)
+	# Second convolutional layer: (8,8,ngf*8) -> (16,16,ngf*4)
+	model.add(layers.Conv2DTranspose(ngf*4, (4, 4), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 16, 16, ngf*4)
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	
-	model.add(layers.Conv2DTranspose(ngf*1, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-	assert model.output_shape == (None, 32, 32, ngf)
+	# Third convolutional layer: (16, 16, ngf*4) -> (32, 32, ngf*2)
+	model.add(layers.Conv2DTranspose(ngf*2, (4, 4), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 32, 32, ngf*2)
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-
-	model.add(layers.Conv2DTranspose(nc, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-	assert model.output_shape == (None, 64, 64, nc)
+	# Fourth convolutional layer: (32, 32, ngf*2) -> (64, 64, ngf)
+	model.add(layers.Conv2DTranspose(ngf, (4, 4), strides=(2, 2), padding='same', use_bias=False))
+	assert model.output_shape == (None, 64, 64, ngf)
+	model.add(layers.BatchNormalization())
+	model.add(layers.ReLU())
+	# Fifth convolutional layer: (64, 64, ngf) -> (128, 128, nc)
+	model.add(layers.Conv2DTranspose(nc, (4, 4), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+	assert model.output_shape == (None, 128, 128, nc)
 
 	return model
 
-
-# In[5]:
-
-
-# use the untrained generator to generate an image
-print("creating generator...")
-generator = make_generator_model()
-noise = tf.random.normal([1,nz])
-generated_image = generator(noise, training=False)
-print(generated_image.shape)
-# plt.imshow((generated_image.numpy()[0, :, :, :] * 127.5 + 127.5).astype(int))
-
-
-# In[6]:
-
-
 # Discriminator
 def make_discriminator_model():
+	ndf = FLAGS.ndf
+	nc = FLAGS.nc
 	model = tf.keras.Sequential()
-	model.add(layers.Conv2D(ndf, (5, 5), strides=(2, 2), padding='same', input_shape=[64, 64, nc]))
-	model.add(layers.LeakyReLU())
-	assert model.output_shape == (None, 32, 32, ndf)
+	model.add(layers.Conv2D(ndf, (4, 4), strides=(2, 2), padding='same', input_shape=[128, 128, nc]))
+	model.add(layers.LeakyReLU(alpha=FLAGS.alpha))
+	assert model.output_shape == (None, 64, 64, ndf)
 
-	model.add(layers.Conv2D(ndf*2, (5, 5), strides=(2, 2), padding='same'))
+	model.add(layers.Conv2D(ndf*2, (4, 4), strides=(2, 2), padding='same'))
 	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-	assert model.output_shape == (None, 16, 16, ndf*2)
+	model.add(layers.LeakyReLU(alpha=FLAGS.alpha))
+	assert model.output_shape == (None, 32, 32, ndf*2)
 
-	model.add(layers.Conv2D(ndf*4, (5, 5), strides=(2, 2), padding='same'))
-	print(model.output_shape)
+	model.add(layers.Conv2D(ndf*4, (4, 4), strides=(2, 2), padding='same'))
 	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-	assert model.output_shape == (None, 8, 8, ndf*4)
+	model.add(layers.LeakyReLU(alpha=FLAGS.alpha))
+	assert model.output_shape == (None, 16, 16, ndf*4)
 
-	model.add(layers.Conv2D(ndf*8, (5, 5), strides=(2, 2), padding='same'))
+	model.add(layers.Conv2D(ndf*8, (4, 4), strides=(2, 2), padding='same'))
 	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-	assert model.output_shape == (None, 4, 4, ndf*8)
+	model.add(layers.LeakyReLU(alpha=FLAGS.alpha))
+	assert model.output_shape == (None, 8, 8, ndf*8)
+
+	model.add(layers.Conv2D(ndf*16, (4, 4), strides=(2, 2), padding='same'))
+	model.add(layers.BatchNormalization())
+	model.add(layers.LeakyReLU(alpha=FLAGS.alpha))
+	assert model.output_shape == (None, 4, 4, ndf*16)
 	
 	model.add(layers.Flatten())
 	model.add(layers.Dense(1))
 
 	return model
 
-
-# In[7]:
-
-print("creating discriminator...")
-discriminator = make_discriminator_model()
-decision = discriminator(generated_image)
-print(decision)
-
-
-# In[8]:
-
-
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-
-# In[9]:
-
 
 # Define losses
 def discriminator_loss(real_output, fake_output):
@@ -153,37 +121,18 @@ def generator_loss(fake_output):
 	return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 # Optimizers
-generator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1)
+generator_optimizer = tf.keras.optimizers.Adam(FLAGS.lr, FLAGS.beta_1)
+discriminator_optimizer = tf.keras.optimizers.Adam(FLAGS.lr, FLAGS.beta_1)
 
-
-# In[10]:
-
-
-# Save checkpoints
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-								 discriminator_optimizer=discriminator_optimizer,
-								 generator=generator,
-								 discriminator=discriminator)
-
-
-# In[11]:
-
-
-# We will reuse this seed overtime (so it's easier)
+# We will reuse this noise overtime (so it's easier)
 # to visualize progress in the animated GIF)
-seed = tf.random.normal([num_samples, nz])
-
-
-# In[12]:
-
+noise = tf.random.normal([FLAGS.num_samples, FLAGS.nz])
 
 # Notice the use of `tf.function`
 # This annotation causes the function to be "compiled".
 @tf.function
 def train_step(images):
-	noise = tf.random.normal([batch_size, nz])
+	noise = tf.random.normal([FLAGS.batch_size, FLAGS.nz])
 
 	with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
 		generated_images = generator(noise, training=True)
@@ -202,43 +151,44 @@ def train_step(images):
 	
 	return gen_loss, disc_loss
 
-def train(dataset, epochs):
-	for epoch in range(75, epochs):
-		start = time.time()
+def train(dataset, start_epoch=0, epochs=100):
+	batch_size = FLAGS.batch_size
+	for epoch in range(start_epoch, epochs):
+		start = time()
 		batch_idxs = len(dataset) // batch_size # number of batches in train dataset
 		for idx in range(batch_idxs):
-			step_time = time.time()
+			step_time = time()
 			image_batch = util.get_images(dataset[idx*batch_size:(idx+1)*batch_size])
 			gen_loss, disc_loss = train_step(image_batch)
 			print("Epoch: [{}/{}] [{}/{}] took: {:.3f}, d_loss: {:.5f}, g_loss: {:.5f}".format(
-				epoch+1, epochs, idx+1, batch_idxs, time.time()-step_time, disc_loss, gen_loss))
+				epoch+1, epochs, idx+1, batch_idxs, time()-step_time, disc_loss, gen_loss))
 
 		# Produce images for the GIF as we go
 		generate_and_save_images(generator,
-								 epoch + 1,
-								 seed)
+							   noise,
+							   output_dir=FLAGS.output_dir,
+							   image_name='image_at_epoch_{:04d}.png'.format(epoch+1))
 
 		# Save the model after every number of epochs
-		if (epoch+1) % save_every == 0:
+		if (epoch+1) % FLAGS.save_every == 0:
 			print("saving checkpoint at", epoch+1)
 			checkpoint.save(file_prefix = checkpoint_prefix)
 
-		print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+		print ('Time for epoch {} is {} sec'.format(epoch + 1, time()-start))
 
 	# Generate after the final epoch
 	generate_and_save_images(generator,
-						   epochs,
-						   seed)
-
-
-# In[13]:
+						   noise,
+						   output_dir=FLAGS.output_dir,
+						   image_name='image_at_epoch_{:04d}.png'.format(epoch+1))
 
 
 # Generate and save images
-def generate_and_save_images(model, epoch, test_input):
+def generate_and_save_images(model, noise, output_dir, image_name):
 	# Notice `training` is set to False.
 	# This is so all layers run in inference mode (batchnorm).
-	predictions = model(test_input, training=False)
+	predictions = model(noise, training=False)
+	print(predictions.shape)
 
 	fig = plt.figure(figsize=(8,8))
 
@@ -247,21 +197,50 @@ def generate_and_save_images(model, epoch, test_input):
 		plt.imshow((predictions.numpy()[i, :, :, :] * 127.5 + 127.5).astype(int)) # put image on [0,255] range
 		plt.axis('off')
 
-	plt.savefig(output_dir + '/' + 'image_at_epoch_{:04d}.png'.format(epoch))
+	# make the directory if it is not already present
+	if not os.path.exists(output_dir):
+		os.makedirs(output_dir)
+	plt.savefig(output_dir + '/' + image_name)
 	plt.clf()
 
-# In[ ]:
+if __name__ == '__main__':
 
+	# load the data
+	if FLAGS.train:
+		print("loading data from {} dataset...".format(FLAGS.dataset))
+		train_images = util.load_data(FLAGS.data_path, FLAGS.dataset)
+		print(len(train_images), "images loaded.")
 
-# Train the model
-if restore_from_checkpoint:
-	checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+	# Create the generator and discriminator
+	print("creating generator...")
+	test_input = tf.random.normal([1,FLAGS.nz])
+	generator = make_generator_model()
+	generated_image = generator(test_input, training=False) # use the untrained generator to generate an image
+	print(generated_image.shape)
 
-train(train_images, epochs)
+	print("creating discriminator...")
+	discriminator = make_discriminator_model()
+	decision = discriminator(generated_image) # get the untrained discriminator decision on generated image
+	print(decision)
 
+	# Save checkpoints
+	checkpoint_prefix = os.path.join(FLAGS.checkpoint_dir, "ckpt")
+	checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+									 discriminator_optimizer=discriminator_optimizer,
+									 generator=generator,
+									 discriminator=discriminator)
 
-# In[ ]:
+	# Train the model
+	if FLAGS.restore_from_checkpoint: # restore from the latest chec
+		checkpoint.restore(tf.train.latest_checkpoint(FLAGS.checkpoint_dir))
 
+	if FLAGS.train:
+		train(train_images, FLAGS.start_epoch, FLAGS.epochs)
+
+	if FLAGS.generate:
+		if FLAGS.generated_dir == None:
+			raise Exception("generated_dir flag not defined.")
+		generate_and_save_images(generator, noise, FLAGS.generated_dir, image_name="Generated Image {}.png".format(ctime()))
 
 
 
--- model/util.py
+++ model/util.py
@@ -1,29 +1,52 @@
 # import necessary libraries
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 import os
 from glob import glob
 import csv
+from absl import flags, app
 
-# get all image files in directory specified by from_path and store resized images into to_path
-def resize_and_save_images(from_path, save_path, fname_pattern="*.jpg", output_size=64):
-	img_files = glob(os.join(from_path, fname_pattern))
-	for filename in img_files:
-		# resize image
-		image = Image.open(filename)
-		image = image.resize((output_size, output_size))
-		# save in new directory
-		image.save(os.join(save_path, os.path.basename(filename)))
+FLAGS = flags.FLAGS
+# flags for extracting genre from wikiart
+flags.DEFINE_boolean('get_artworks_by_genre', False, "Extract all artworks of a specific genre")
+flags.DEFINE_integer('genre', 1, "Integer specifying which genre to extract")
+flags.DEFINE_string('wikiart_from_path', '../../wikiart', "Path to wikiart directory")
+# flags for augmenting images
+flags.DEFINE_boolean('augment', False, "Augment all images specified in a directory")
+# flags for resizing and saving images
+flags.DEFINE_boolean('resize_and_save_images', False, "Resize and save all images in a directory")
+# used for multiple settings
+flags.DEFINE_string('from_path', None, "Path to dataset of images to augment")
+flags.DEFINE_string('save_path', None, "Path to directory to save resized images")
+flags.DEFINE_integer('output_size', None, "Resize images to this size")
+
+def load_data(from_path, dataset, fname_pattern='*.jpg'):
+	path_name = os.path.join(from_path, dataset, fname_pattern)
+	imgs = glob(path_name)
+	np.random.shuffle(imgs)
+	return imgs
+
+def get_images(image_files, output_size=128):
+	images = []
+	for fname in image_files:
+		image = (Image.open(fname))
+		width, height = image.size
+		crop_size = min(width, height) # set the square crop side length to the minimum of width and height
+		image = image.crop((0,0,crop_size, crop_size)) # crop image
+		image = image.resize(size=(output_size, output_size), resample=Image.BILINEAR) # resize image
+		images.append((np.asarray(image) - 127.5)/127.5)
+	return np.array(images, dtype='float32')
 
 # resize and save a singular image at file_path to save_path directory
-def resize_and_save_image(file_path, save_path, output_size=64):
+def resize_and_save_image(file_path, save_path, output_size=None):
 	filename = os.path.basename(file_path)
 	print('processing', filename)
 	image = Image.open(file_path)
-	image = image.resize((output_size, output_size))
+	if output_size != None:
+		image = image.resize((output_size, output_size))
 	image.save(os.path.join(save_path, filename))
 
-def get_artworks_by_genre(genre, database_path, csv_path, save_path, output_size=64):
+def get_artworks_by_genre(genre, database_path, csv_path, save_path, output_size=None):
 	print('Database', database_path)
 	print('CSV', csv_path)
 	print('Save', save_path)
@@ -33,24 +56,76 @@ def get_artworks_by_genre(genre, database_path, csv_path, save_path, output_size
 		for row in csv_reader:
 			# if this painting is part of the specified genre, resize and save in target directory
 			if int(row[1]) == genre:
-				resize_and_save_image(os.path.join(database_path, row[0]), save_path)
+				resize_and_save_image(os.path.join(database_path, row[0]), save_path, output_size)
 
-def load_data(data_path, dataset_name, fname_pattern='*.jpg'):
-	path_name = os.path.join(data_path, dataset_name, fname_pattern)
-	return glob(path_name)
+# load image
+img_cnt = 0
+img_trans = 0
+def augment_data(from_path, save_path):
+	global img_cnt
+	global img_trans
+	images = glob(os.path.join(from_path, '*.jpg'))
+	for image in images:
+		with open(image, 'rb') as file:
+			img = Image.open(file)
+			flip_and_mirror(img,128,save_path)
+			img_rot_90 = img.rotate(90)
+			flip_and_mirror(img_rot_90,128,save_path)
+			img_cnt += 1
+			img_trans = 0
 
-def get_images(image_files):
-	images = []
-	for fname in image_files:
-		img_data = np.asarray(Image.open(fname))
-		images.append((img_data - 127.5)/127.5)
-	return np.array(images, dtype='float32')
+def flip_and_mirror(img, size, save_path):
+	do_cropping(img,size, save_path)  
+	do_cropping(ImageOps.flip(img),size, save_path)  
+	do_cropping(ImageOps.mirror(img),size, save_path)  
+	do_cropping(ImageOps.flip(ImageOps.mirror(img)),size, save_path)  
 
+def do_cropping(img, size, save_path):
+	save(img, save_path)
+	width, height = img.size
+	img_size = min(width, height)
+	loops = img_size // size
+	for i in range(loops):
+		for j in range(loops):
+			cropped = img.crop((i*size, j*size, i*size+size, j*size+size))
+			save(cropped, save_path)
 
-# CODE FOR CREATING A FOLDER OF ALL PAINTINGS BELONGING TO A GENRE
-# wikiart_data_path = '../../wikiart-data'
+def save(img, save_path):
+	global img_trans
+	img.save(save_path + '/'+ str(img_cnt) + '_' +str(img_trans) + '.jpg')
+	img_trans += 1
 
-# get_artworks_by_genre(genre=4, 
-# 	database_path=os.path.join(wikiart_data_path,'wikiart'), 
-# 	csv_path=os.path.join(wikiart_data_path,'wikiart_csv/genre_train.csv'), 
-# 	save_path=os.path.join(wikiart_data_path,'Landscapes'))
+# get all image files in directory specified by from_path and store resized images into to_path
+def resize_and_save_images(from_path, save_path, fname_pattern="*.jpg", output_size=64):
+	img_files = glob(os.join(from_path, fname_pattern))
+	for filename in img_files:
+		# resize image
+		image = Image.open(filename)
+		if output_size != None:
+			image = image.resize((output_size, output_size))
+		# save in new directory
+		image.save(os.join(save_path, os.path.basename(filename)))
+
+def transfer_images(image_names_path, abstract_path, save_path):
+	image_names = glob(os.path.join(image_names_path, "*.jpg"))
+	for image_name in image_names:
+		filename = os.path.basename(image_name)
+		image = Image.open(os.path.join(abstract_path, filename))
+		image.save(os.path.join(save_path, filename))
+
+def main(argv):
+	if FLAGS.get_artworks_by_genre:
+			get_artworks_by_genre(genre=FLAGS.genre, 
+				database_path=os.path.join(FLAGS.wikiart_from_path,'wikiart'), 
+				csv_path=os.path.join(FLAGS.wikiart_from_path,'wikiart_csv/genre_train.csv'), 
+				save_path=FLAGS.save_path,
+				output_size=FLAGS.output_size)
+	
+	if FLAGS.augment:
+		augment_data(FLAGS.from_path, FLAGS.save_path)
+
+	if FLAGS.resize_and_save_images:
+		resize_and_save_images(FLAGS.from_path, FLAGS.save_path, output_size=FLAGS.output_size)
+
+if __name__ == '__main__':
+	app.run(main)
